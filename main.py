2f3f729784e31a516d994c5f593978c08b3ebb52
mariodragun <mariodragun1982@gmail.com>
2021-04-24 19:58:13 +0000

    defining questions and answers models

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from enum import unique
 from flask import Flask
 import os
 
 from flask_mongoengine import MongoEngine
 import mongoengine as me
+import datetime
 
 # init flask app
 app = Flask(__name__)
@@ -34,3 +34,28 @@ class User(me.Document):
     password = me.StringField(max_length=256)
 
 
+# Question - list of answers (one or many can be true)
+class Answer(me.EmbeddedDocument):
+    # answer basic text
+    answer = me.StringField(required=True)
+    # is correct boolean, will signify if the answer is correct or not
+    is_correct = me.BooleanField(default=False)
+
+    # standard datetime field which will be updated every time Question is modified
+    # and that is why the default value datetime.datetime.now which is the now date/time (server time)
+    date_modified = me.DateTimeField(default=datetime.datetime.now)
+
+
+class Question(me.Document):
+    # stanard question text title
+    title = me.StringField(required=True, unique=True)
+    # help text for the question - additional explanation of the question, not required
+    description = me.StringField(default="")
+    # url field for the images, also additional explanations of the questions
+    image = me.URLField(required=False)
+    # embeded document, one question can have many answers (List of answers)
+    answers = me.ListField(me.EmbeddedDocumentField(Answer))
+
+    # standard datetime field which will be updated every time Question is modified
+    # and that is why the default value datetime.datetime.now which is the now date/time (server time)
+    date_modified = me.DateTimeField(default=datetime.datetime.now)
