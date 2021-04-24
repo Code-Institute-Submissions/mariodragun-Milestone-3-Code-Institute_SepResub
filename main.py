268207c2e4d65ea8cfdbfd4213caf8a41582f8aa
mariodragun <mariodragun1982@gmail.com>
2021-04-24 19:33:56 +0000

    defining database for users

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from enum import unique
 from flask import Flask
 import os
 
-
 from flask_mongoengine import MongoEngine
+import mongoengine as me
 
 # init flask app
 app = Flask(__name__)
@@ -15,3 +16,21 @@ app.config["MONGODB_HOST"] = os.getenv("MONGODB_HOST")
 # init mongo database and attach it to the app
 db = MongoEngine()
 db.init_app(app)
+
+
+#### DB Models
+# define db models to create collections in MongoDB
+# define basic User Model - to register/login or use User information
+# use me as mongoengine alias. And for each field define what type it is.
+class User(me.Document):
+    first_name = me.StringField(required=True)
+    last_name = me.StringField(required=True)
+    #  username is set to be unique to prevent multiple users to have same username
+    username = me.StringField(required=True, unique=True)
+    # email is set to be unique to prevent multiple users to have same email
+    email = me.EmailField(required=True, unique=True)
+    # password is set to be a plain text field - because we will use generate_password_hash so
+    #  there is no need to hash it again
+    password = me.StringField(max_length=256)
+
+
