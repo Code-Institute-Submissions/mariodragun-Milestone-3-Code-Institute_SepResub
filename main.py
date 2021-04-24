e8abef21f542db629b470902fc550c84b7ab35b1
mariodragun <mariodragun1982@gmail.com>
2021-04-24 18:56:42 +0000

    data base configuration

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from flask import Flask
 import os
+
+
+from flask_mongoengine import MongoEngine
+
+# init flask app
+app = Flask(__name__)
+
+# adding configuration
+app.config["SECRET_KEY"] = os.getnv("SECRET_KEY")
+# add configuration for MongoDB Cluster
+app.config["MONGODB_HOST"] = os.getenv("MONGODB_HOST")
+
+# init mongo database and attach it to the app
+db = MongoEngine()
+db.init_app(app)
