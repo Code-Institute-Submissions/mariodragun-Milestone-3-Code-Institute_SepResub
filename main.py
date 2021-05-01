9b1160a54826f845c1a7e4f957fc23aa685a9e11
mariodragun <mariodragun1982@gmail.com>
2021-05-01 17:19:49 +0000

    setting code to 80 signs

--- main.py
+++ main.py
@@ -35,11 +35,13 @@ db.init_app(app)
 class User(me.Document):
     first_name = me.StringField(required=True)
     last_name = me.StringField(required=True)
-    #  username is set to be unique to prevent multiple users to have same username
+    #  username is set to be unique to prevent multiple users to have same 
+    # username
     username = me.StringField(required=True, unique=True)
     # email is set to be unique to prevent multiple users to have same email
     email = me.EmailField(required=True, unique=True)
-    # password is set to be a plain text field - because we will use generate_password_hash so
+    # password is set to be a plain text field - because we will use 
+    # generate_password_hash so
     #  there is no need to hash it again
     password = me.StringField(max_length=256)
 
@@ -51,8 +53,10 @@ class Answer(me.EmbeddedDocument):
     # is correct boolean, will signify if the answer is correct or not
     is_correct = me.BooleanField(default=False)
 
-    # standard datetime field which will be updated every time Question is modified
-    # and that is why the default value datetime.datetime.now which is the now date/time (server time)
+    # standard datetime field which will be updated every time Question is 
+    # modified
+    # and that is why the default value datetime.datetime.now which is the now 
+    # date/time (server time)
     date_modified = me.DateTimeField(default=datetime.datetime.now)
 
 
@@ -60,15 +64,18 @@ class Answer(me.EmbeddedDocument):
 class Question(me.Document):
     # stanard question text title
     title = me.StringField(required=True, unique=True)
-    # help text for the question - additional explanation of the question, not required
+    # help text for the question - additional explanation of the question, not 
+    # required
     description = me.StringField(default="")
     # url field for the images, also additional explanations of the questions
     image = me.URLField(required=False)
     # embeded document, one question can have many answers (List of answers)
     answers = me.ListField(me.EmbeddedDocumentField(Answer))
 
-    # standard datetime field which will be updated every time Question is modified
-    # and that is why the default value datetime.datetime.now which is the now date/time (server time)
+    # standard datetime field which will be updated every time Question is 
+    # modified
+    # and that is why the default value datetime.datetime.now which is the now 
+    # date/time (server time)
     date_modified = me.DateTimeField(default=datetime.datetime.now)
 
 # Embeded model for QuizTaken - holds questions and chosen answers
@@ -77,15 +84,12 @@ class QuestionsAnswered(me.EmbeddedDocument):
     chosen_answer = me.StringField()
     is_correct = me.BooleanField(default=False)
 
-    def __repr__(self):
-        return f"Question: {self.question.title}, correct: {self.is_correct}"
-
-    def __str__(self):
-        return f"Question: {self.question.title}, correct: {self.is_correct}"
+   
 
 class QuizTaken(me.Document):
     user = me.ReferenceField(User)
-    list_of_questions = me.ListField(me.EmbeddedDocumentField(QuestionsAnswered))
+    list_of_questions = me.ListField(
+        me.EmbeddedDocumentField(QuestionsAnswered))
     number_of_questions = me.IntField()
     correct_answers = me.IntField(default=0)
 
@@ -98,11 +102,7 @@ class QuizTaken(me.Document):
     def overall_score(self):
         return f"{self.correct_answers}/{self.number_of_questions}"
 
-    def __repr__(self):
-        return f"User: {self.user}"
 
-    def __str__(self):
-        return f"User: {self.user}"
 
 
 
@@ -134,33 +134,44 @@ class RegisterForm(Form):
     )
     username = StringField(
         "Username",
-        validators=[validators.Length(min=10, max=150), validators.DataRequired(message="Username is required.")],
+        validators=[
+            validators.Length(min=10, max=150),
+            validators.DataRequired(message="Username is required.")],
     )
-    # password fields hold few validator as also qualTo which will compare value against `confirm_password` field
-    #  in the case that the values are different it will print out the appropriate message which is set here
+    # password fields hold few validator as also qualTo which will compare 
+    # value against `confirm_password` field
+    #  in the case that the values are different it will print out the 
+    # appropriate message which is set here
     password = PasswordField(
         "Password",
         validators=[
             validators.Length(min=10, max=45),
             validators.DataRequired(message="Password is required"),
-            validators.EqualTo(fieldname="confirm_password", message="Entered passwords do not match"),
+            validators.EqualTo(
+                fieldname="confirm_password", 
+                message="Entered passwords do not match"),
         ],
     )
     # confirm password field, which should be the same as the password field
     confirm_password = PasswordField(
         "Confirm Password",
-        validators=[validators.Length(min=10, max=45), validators.DataRequired("Confirm password is required.")],
+        validators=[validators.Length(min=10, max=45), validators.DataRequired(
+            "Confirm password is required.")],
     )
 
 # login form
 class LoginForm(Form):
     username = StringField(
         "Username",
-        validators=[validators.Length(min=10, max=150), validators.DataRequired(message="Username is required")],
+        validators=[
+            validators.Length(min=10, max=150),
+            validators.DataRequired(message="Username is required")],
     )
     password = StringField(
         "Password",
-        validators=[validators.Length(min=10, max=45), validators.DataRequired(message="Password is required.")],
+        validators=[
+            validators.Length(min=10, max=45), 
+            validators.DataRequired(message="Password is required.")],
     )
 
 
@@ -169,16 +180,19 @@ class LoginForm(Form):
 def register():
     #  init Registration form and add request.form data to it
     form = RegisterForm(request.form)
-    #  at the POST request chck if the form is valid (if the initial validation is passed)
+    #  at the POST request chck if the form is valid 
+    # (if the initial validation is passed)
     if request.method == "POST" and form.validate():
-        #  create a new user based on the form data, and also generate password hash
+        #  create a new user based on the form data, 
+        # and also generate password hash
         # to store password as a hash instead of a plain string
         new_user = User(
             first_name=form.first_name.data,
             last_name=form.last_name.data,
             username=form.username.data,
             email=form.email.data,
-            password=generate_password_hash(form.password.data, method="sha256"),
+            password=generate_password_hash(
+                orm.password.data, method="sha256"),
         )
         # save new user
         new_user.save()
@@ -193,7 +207,8 @@ def login():
     form = LoginForm(request.form)
 
     if request.method == "POST" and form.validate():
-        # this returns List and we need only one object and that is why .first() is required
+        # this returns List and we need only one object and that
+        # is why .first() is required
         user = User.objects(username=form.username.data).first()
         # if user exists
         if user:
