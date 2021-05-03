5a722515acc6d4c7c9a1f8a9a50b81a6ebf86b53
mariodragun <mariodragun1982@gmail.com>
2021-05-03 19:52:35 +0000

    repairing bugs

--- main.py
+++ main.py
@@ -270,7 +270,7 @@ def register():
         new_user.save()
         flash("Successfully registered", "success")
         # return success message that registration is done
-        return jsonify({"registration": "done"})
+        return redirect(url_for("login"))
     else:
         return render_template("register.html", form=form)
 
