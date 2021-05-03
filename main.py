15184bd8997f14d0315c6e6df1a4ab6ebff2036d
mariodragun <mariodragun1982@gmail.com>
2021-05-03 18:41:31 +0000

    repairing bugs

--- main.py
+++ main.py
@@ -268,7 +268,8 @@ def register():
         flash("Successfully registered", "success")
         # return success message that registration is done
         return render_template("register.html", form=form)
-    
+    else:
+        return render_template("register.html", form=form)
 
 @app.route("/login/", methods=["GET", "POST"])
 def login():
